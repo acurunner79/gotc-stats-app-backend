2b6d725e3eb10a3633ca98fc8b1e029a888aaa16
acurunner79 <djsmoothzouk@gmail.com>
2022-01-13 22:38:56 -0600

    all crud routes have been written and tested with postman

--- app/controllers/infantries_controller.rb
+++ app/controllers/infantries_controller.rb
@@ -1,56 +1,43 @@
 class InfantriesController < ApplicationController
-  before_action :set_infantry,  only: [:show, :create, :update, :destroy]
+  # before_action :set_infantry, :update_infantry, only: [:show, :create, :update, :destroy]
 
   # GET /infantries
   def index
-    @infantries = Infantry.all
-
-    render json: @infantries
+    render json: {status: 200, infantries: Infantry.all}
   end
 
   # GET /infantries/1
   def show
-    @infantry = Infantry.find(params[:id])
-    # render json: infantry
+    render json: Infantry.find(params[:id])
   end
 
   # POST /infantries
   def create
-    # infantry = Infantry.new(infantry_params)
-    @infantry = Infantry.new(infantry_params)
+    infantry = Infantry.new(infantry_params)
 
-    if @infantry.save
-      render json: @infantry, status: :created
-      # , location: @infantry
+    if infantry.save
+      render status: 201, json: {infantry: infantry}
     else
-      render json: @infantry.errors, status: :unprocessable_entity
+      render status: 422, json: {infantry: infantry} 
     end
   end
 
   # PATCH/PUT /infantries/1
   def update
-    if @infantry.update(infantry_params)
-      render json: @infantry
-    else
-      render json: @infantry.errors, status: :unprocessable_entity
-    end
+    infantry = Infantry.find(params[:id])
+    infantry.update(infantry_params)
+    render(json: { infantry: infantry })
   end
 
   # DELETE /infantries/1
   def destroy
-    @infantry.destroy
+    infantry = Infantry.destroy(params[:id])
+    render(status: 204)
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_infantry
-      @infantry = Infantry.new(params[:id])
-    end
-
-    # def update_infantry
-    #   @infantry = Infantry.update(params[:id])
-    # end
-
+    
     # Only allow a list of trusted parameters through.
     def infantry_params
       params.require(:infantry).permit(:inf_attack, :inf_defense, :inf_health, :inf_attack_vs_player, :inf_defense_vs_player, :inf_health_vs_player, :inf_attack_vs_players_inf, :inf_attack_vs_players_range, :inf_attack_vs_players_cav, :inf_defense_vs_players_cav, :inf_defense_vs_players_inf, :inf_defense_vs_players_range, :inf_defense_vs_players_siege)
--- app/controllers/rangeds_controller.rb
+++ app/controllers/rangeds_controller.rb
@@ -1,48 +1,42 @@
 class RangedsController < ApplicationController
-  before_action :set_ranged, only: [:show, :update, :destroy]
+  # before_action :set_ranged, only: [:show, :update, :destroy]
 
   # GET /rangeds
   def index
-    @rangeds = Ranged.all
-
-    render json: @rangeds
+    render json: { status: 200, rangeds: Ranged.all }
   end
 
   # GET /rangeds/1
   def show
-    render json: @ranged
+    render json: Ranged.find(params[:id])
   end
 
   # POST /rangeds
   def create
-    @ranged = Ranged.new(ranged_params)
+    ranged = Ranged.new(ranged_params)
 
-    if @ranged.save
-      render json: @ranged, status: :created, location: @ranged
+    if ranged.save
+      render status: 201 json: { ranged: ranged }
     else
-      render json: @ranged.errors, status: :unprocessable_entity
+      render status: 422, json: { ranged: ranged }
     end
   end
 
   # PATCH/PUT /rangeds/1
   def update
-    if @ranged.update(ranged_params)
-      render json: @ranged
-    else
-      render json: @ranged.errors, status: :unprocessable_entity
-    end
+    ranged = Ranged.find(params[:id])
+    ranged.update(ranged_params)
+    render(json: { ranged: ranged })
   end
 
   # DELETE /rangeds/1
   def destroy
-    @ranged.destroy
+    ranged = Ranged.destroy(params[:id])
+    render(status: 204)
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_ranged
-      @ranged = Ranged.find(params[:id])
-    end
 
     # Only allow a list of trusted parameters through.
     def ranged_params
